3b8bb00247fa2b7c1328ac9a29ff1f3b62d6d083
Antoine <antoine.ngu@outlook.fr>
2019-05-12 22:12:24 +0200

    Trying to set homepage

--- apps/bracelet/assets/js/app.js
+++ apps/bracelet/assets/js/app.js
@@ -1,15 +1,47 @@
 // CSS
 import '../css/global.scss';
-import '../css/app.css';
 
 // Images
 import '../images/favicon.ico';
+import '../images/bg-img/1.png';
+import '../images/bg-img/2.jpg';
+import '../images/bg-img/3.jpg';
+import '../images/bg-img/4.jpg';
+import '../images/bg-img/5.jpg';
+import '../images/bg-img/6.jpg';
+import '../images/bg-img/7.jpg';
+import '../images/bg-img/8.jpg';
+import '../images/bg-img/9.jpg';
+import '../images/bg-img/10.jpg';
+import '../images/bg-img/11.jpg';
+import '../images/bg-img/12.jpg';
+import '../images/bg-img/13.jpg';
+import '../images/bg-img/14.jpg';
+import '../images/bg-img/15.jpg';
+import '../images/bg-img/16.jpg';
+import '../images/bg-img/17.jpg';
+import '../images/bg-img/18.jpg';
+import '../images/bg-img/19.jpg';
+import '../images/bg-img/20.jpg';
+import '../images/bg-img/21.jpg';
+import '../images/bg-img/22.jpg';
+import '../images/bg-img/23.jpg';
+import '../images/bg-img/24.jpg';
+import '../images/core-img/curve-1.png';
+import '../images/core-img/curve-2.png';
+import '../images/core-img/curve-3.png';
+import '../images/core-img/curve-4.png';
+import '../images/core-img/curve-5.png';
+import '../images/core-img/curve-6.png';
+import '../images/core-img/logo.png';
 
 // JS
 import $ from 'jquery';
 import 'popper.js';
 import 'bootstrap';
 import 'select2';
+import './uza.bundle';
+import './active';
 
 $(document).ready(function () {
   $('.select2').select2();
