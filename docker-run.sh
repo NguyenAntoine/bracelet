d661b9c173ca223fbd8de843fb1b94cffc0ad057
Antoine <antoine.ngu@outlook.fr>
2019-05-25 02:41:30 +0200

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-docker-compose down
+docker-compose down -f docker-compose-production.yml
 docker rmi bracelet_php
-docker-compose build
-docker-compose up -d
+docker-compose build -f docker-compose-production.yml
+docker-compose up -f docker-compose-production.yml -d
